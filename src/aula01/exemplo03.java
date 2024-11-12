55e90d14e0ec62901275ad548f29ab39bbef6e48
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-12 11:27:24 -0300

    init commit

---
+++ src/aula01/exemplo03.java
@@ -0,0 +1,17 @@
+package aula01;
+
+public class exemplo03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10;
+		int b = -10;
+		boolean c  = false;
+		boolean d = true;
+		System.out.println(~a);
+		System.out.println(~b);
+		System.out.println(!c);
+		System.out.println(!d);
+	}
+
+}
