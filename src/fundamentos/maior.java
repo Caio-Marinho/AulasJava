3d40612435a3e4db0db004b3c7306d879ef74662
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-14 09:38:16 -0300

    quem é o maior

--- src/fundamentos/maior.java
+++ src/fundamentos/maior.java
@@ -1,28 +1,24 @@
-package fundamentos;
-import java.util.Scanner;
-
-public class maior {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner numero = new Scanner (System.in);
-		System.out.print("Diga o 1º número: ");
-		double n1 = numero.nextDouble();
-		System.out.print("Diga o 2º número: ");
-		double n2 = numero.nextDouble();
-		System.out.print("Diga o 3º número: ");
-		double n3 = numero.nextDouble();
-		if (n1<n2) {
-			if (n2<n3) {
-				System.out.print(n3+" é o maior");
-			}else {
+	package fundamentos;
+	import java.util.Scanner;
+	
+	public class maior {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner numero = new Scanner (System.in);
+			System.out.print("Diga o 1º número: ");
+			double n1 = numero.nextDouble();
+			System.out.print("Diga o 2º número: ");
+			double n2 = numero.nextDouble();
+			System.out.print("Diga o 3º número: ");
+			double n3 = numero.nextDouble();
+			if (n2<n1 && n3<n1) {
+				System.out.print(n1+" é o maior");
+			} else  if (n1<n2 && n3<n2) {
 				System.out.print(n2+" é o maior");
+			} else {
+				System.out.print(n3+" é o maior");
 			}
 			
-		}else {
-			System.out.print(n1+" é o maior");
-		}
-
 	}
-
 }
