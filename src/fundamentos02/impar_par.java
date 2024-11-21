0741a4cb00e1e3fe408730b73fee88a4b510b0a5
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-21 10:45:27 -0300

    impares com entrada

---
+++ src/fundamentos02/impar_par.java
@@ -0,0 +1,22 @@
+package fundamentos02;
+
+public class impar_par {
+	public static void main(String[] args) {
+		int num =1;
+		
+		while(num<=100) {
+			if (num%2!=0) {
+				System.out.print(num+",");
+			}
+			num++;
+		}
+		num = 1;
+		System.out.println();
+		while(num<=100) {
+			if (num%2 == 0) {
+				System.out.print(num+",");
+			}
+			num++;
+		}
+	}
+}
