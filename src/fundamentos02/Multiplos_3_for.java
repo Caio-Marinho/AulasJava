4998ec0180fdd6120e0444af2231ef6d4b97440a
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-21 11:17:05 -0300

    for laço multiplo

---
+++ src/fundamentos02/Multiplos_3_for.java
@@ -0,0 +1,11 @@
+package fundamentos02;
+
+public class Multiplos_3_for {
+	public static void main(String[] args) {
+		for (int i = 0;i<=100;i++) {
+			if (i % 3 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
