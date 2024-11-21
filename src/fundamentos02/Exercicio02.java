90a359930f4307f69b823b867dd3a52d8c49c753
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-21 09:15:51 -0300

    media  de alunos

--- src/fundamentos02/Exercicio02.java
+++ src/fundamentos02/Exercicio02.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 
 public class Exercicio02 {
 	public static void main(String[] args) {
-		float soma = 0;
-		int cont = 1;
+		int cont = 1, qtd_alunos;
+		float media,nota=0;
 		Scanner entrada = new Scanner(System.in);
 		System.out.print("Informe  o numero de alunos: ");
-		int qtd_alunos = entrada.nextInt();
+		qtd_alunos = entrada.nextInt();
 		while (cont<=qtd_alunos) {
 			System.out.printf("Informe a  nota  do %dº aluno: ",cont);
-			float nota = entrada.nextFloat();
+			nota += entrada.nextFloat();
 			cont++;
-			soma+=nota;
 		}
-		float media = soma/qtd_alunos;
+		media = nota/qtd_alunos;
 		System.out.printf("A média da turma foi %.2f",media);
 		entrada.close();
 	}
