aef5ec10a05d7551900a845cd665572d04b1d27e
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-21 09:01:15 -0300

    media  de alunos

---
+++ src/fundamentos02/Exercicio02.java
@@ -0,0 +1,22 @@
+package fundamentos02;
+import java.util.Scanner;
+
+public class Exercicio02 {
+	public static void main(String[] args) {
+		float soma = 0;
+		int cont = 1;
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Informe  o numero de alunos: ");
+		int qtd_alunos = entrada.nextInt();
+		while (cont<=qtd_alunos) {
+			System.out.printf("Informe a  nota  do %dº aluno: ",cont);
+			float nota = entrada.nextFloat();
+			cont++;
+			soma+=nota;
+		}
+		
+		float media = soma/qtd_alunos;
+		System.out.printf("A média da turma foi %.2f",media);
+		entrada.close();
+	}
+}
---
+++ src/fundamentos02/Laco_while.java
@@ -0,0 +1,11 @@
+package fundamentos02;
+
+public class Laco_while {
+	public static void main(String[] args) {
+		int i = 0;
+		while (i<10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
