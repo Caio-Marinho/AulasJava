886becbd2fb3205d47c881544fd5b8fda2676907
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-21 09:39:10 -0300

    impares com entrada

--- src/fundamentos02/Impares.java
+++ src/fundamentos02/Impares.java
@@ -1,9 +1,14 @@
 package fundamentos02;
+import java.util.Scanner;
 
 public class Impares {
 	public static void main(String[] args) {
-		int numero=1;
-		while(numero<=100) {
+		int numero_inicio,numero=1;
+		Scanner numero_max = new Scanner(System.in);
+		System.out.print("Informe um número: ");
+		numero_inicio = numero_max.nextInt();
+		numero_max.close();
+		while(numero<=numero_inicio) {
 			if (numero % 2 == 1) {
 				System.out.println(numero);
 			}
