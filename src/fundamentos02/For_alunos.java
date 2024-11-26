87d63bbe5f0d4ea27e470fbc866a2ccb5e744bc1
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-26 10:02:24 -0300

    for para media

--- src/fundamentos02/For_alunos.java
+++ src/fundamentos02/For_alunos.java
@@ -1,10 +1,15 @@
 package fundamentos02;
 
+import java.util.Scanner;
+
 public class For_alunos {
 
 	public static void main(String[] args) {
+		int qtd_aluno = 0;
 		// TODO Auto-generated method stub
-
+		Scanner entrada = new Scanner (System.in);
+		System.out.print("Informe quantos alunos tem na sala: ");
+		qtd_a
 	}
 
 }
