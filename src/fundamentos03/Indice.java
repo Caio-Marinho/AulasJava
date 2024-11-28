b43c37acbf0db66267639304e1052c516f4d7fcd
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-28 09:05:16 -0300

    posição do caracter

---
+++ src/fundamentos03/Indice.java
@@ -0,0 +1,12 @@
+package fundamentos03;
+
+public class Indice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hello World World2"; 
+		int pos = str.indexOf ("l");
+		System.out.println(pos);
+	}
+
+}
