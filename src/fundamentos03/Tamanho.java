9ecdfdcc96796a3ce8aed6ca1050e0d5733544e1
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-28 09:04:03 -0300

    tamanho da string

---
+++ src/fundamentos03/Tamanho.java
@@ -0,0 +1,12 @@
+package fundamentos03;
+
+public class Tamanho {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "abc"; 
+		int tam = s.length( );
+		System.out.println(tam);
+	}
+
+}
