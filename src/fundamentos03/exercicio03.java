4c4fb5f567c614bf1786fb3f8f41a406ff4b4301
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-28 09:38:48 -0300

    exercicio 03 comparação

---
+++ src/fundamentos03/exercicio03.java
@@ -0,0 +1,12 @@
+package fundamentos03;
+
+public class exercicio03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String texto01 = "Será que são iguais?";
+		String texto02 = "Será que são iguais?";
+		System.out.println(texto01.equals(texto02) ? true : false);
+	}
+
+}
