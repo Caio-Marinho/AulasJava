595ea6c8e818174897ec4fe6742549b3b7fc429f
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-28 08:39:02 -0300

    concat

---
+++ src/fundamentos03/conct.java
@@ -0,0 +1,11 @@
+package fundamentos03;
+
+public class conct {
+	public static void main(String[] args) {
+		String str = "Oi";String texto = str + " Mundo";
+		System.out.println(texto);
+		int tres = 3;
+		String resultadoFinal = 3 + " palavra " + tres;
+		System.out.println(resultadoFinal);
+	}
+}
