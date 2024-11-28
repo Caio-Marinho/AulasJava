19882f80d7f118c10119963b8595d3dde67b7278
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-28 08:27:41 -0300

    exemplo 01 str

---
+++ src/fundamentos03/Explo01_f3.java
@@ -0,0 +1,9 @@
+package fundamentos03;
+
+public class Explo01_f3 {
+	public static void main(String[] args) {
+		String str = "Hello";
+		String resultado = str.replace("l", "w");
+		System.out.println(resultado);
+	}
+}
