faf25e019377ff3969a94bf524861ece663aed7e
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-28 08:58:42 -0300

    pegar posição de um texto

---
+++ src/fundamentos03/Posicao_texto.java
@@ -0,0 +1,9 @@
+package fundamentos03;
+
+public class Posicao_texto {
+	public static void main(String[] args) {
+		String str = "Hello"; 
+		char c = str.charAt (1); 
+		System.out.println(c);
+	}
+}
