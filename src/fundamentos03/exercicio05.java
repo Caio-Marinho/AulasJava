a41e5ae31235da3f4fdd2fa40b298e43eff4abf9
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-28 10:55:24 -0300

    imprimir texto concat

--- src/fundamentos03/exercicio05.java
+++ src/fundamentos03/exercicio05.java
@@ -4,7 +4,7 @@ public class exercicio05 {
 	public static void main(String[] args) {
 		String array[] = {"a","vida","é","bela"},texto=new String (),espaco=" ";
 		for (int i=0;i<array.length;i++) {
-			texto+=espaco+array[i];
+			texto+=array[i]+espaco;
 		}
 		System.out.println(texto.trim().toUpperCase());
 	}
--- src/fundamentos03/inversao.java
+++ src/fundamentos03/inversao.java
@@ -5,7 +5,7 @@ public class inversao {
 	public static void main(String[] args) {
 		String array[] = {"a","vida","é","bela"},texto=new String (),espaco=" ";
 		for (int i=array.length-1;i>=0;i--) {
-			texto+=espaco+array[i];
+			texto+=array[i]+espaco;
 		}
 		System.out.println(texto.trim().toUpperCase());
 	}
