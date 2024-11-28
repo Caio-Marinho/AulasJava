9a2655c8db73af3527c395fa0f71b28c181315a0
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-28 09:25:41 -0300

    exercicio 01 remoção  de espaço

---
+++ src/fundamentos03/exercicio01.java
@@ -0,0 +1,13 @@
+package fundamentos03;
+
+public class exercicio01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String texto = " texto para retirar espaços no início e fim ";
+		String resultado = texto.trim();
+		System.out.println(resultado+" "+resultado.length());
+		System.out.println(texto+" "+texto.length());
+	}
+
+}
