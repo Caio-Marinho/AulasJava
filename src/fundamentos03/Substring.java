c7acd86ee7ef3f7b2de36d51524b7709ae0eb71a
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-28 08:43:55 -0300

    substring

---
+++ src/fundamentos03/Substring.java
@@ -0,0 +1,12 @@
+package fundamentos03;
+
+public class Substring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hello World";
+		String resultado = str.substring(6);
+		System.out.println("resultado");
+	}
+
+}
