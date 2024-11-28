a04793a23518dc32400d3fe753c9a660a4a02d97
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-28 08:57:29 -0300

    remoção de espaço

---
+++ src/fundamentos03/Espaco.java
@@ -0,0 +1,9 @@
+package fundamentos03;
+
+public class Espaco {
+	public static void main(String[] args) {
+		String str = " Hello "; 
+		String resultado = str.trim ( );
+		System.out.println(resultado);
+	}
+}
