60f507c9e8a14b9d2970000fdfcebd00d218cde6
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-30 08:18:16 -0300

    aplicando o scanner

--- src/JavaPOO/ClasseTeste.java
+++ src/JavaPOO/ClasseTeste.java
@@ -7,7 +7,6 @@ public class ClasseTeste {
 		Scanner entrada = new Scanner(System.in);
 		System.out.print("Informe seu nome: ");
 		aluno.nome = entrada.nextLine();
-		System.out.println(aluno.nome);
 		aluno.comer();
 	}
 }
