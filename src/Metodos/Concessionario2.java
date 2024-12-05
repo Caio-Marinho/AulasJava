9c3233fa74937eee48b9055a70cc9c79fa73b3bc
Caio-Marinho <kaigabriel12@gmail.com>
2024-12-05 09:28:47 -0300

    Herança-arquivo main

--- src/Metodos/Concessionario2.java
+++ src/Metodos/Concessionario2.java
@@ -1,5 +1,8 @@
 package Metodos;
 
 public class Concessionario2 {
-
+	public static void main(String[] args) {
+		Honda hondafitpreto = new Honda("2.0 flex","Honda Accord",60000);
+		System.out.println(hondafitpreto.modelo);
+	}
 }
--- src/Metodos/Honda.java
+++ src/Metodos/Honda.java
@@ -1,5 +1,14 @@
 package Metodos;
 
-public class Honda {
-
+public class Honda extends Carro{
+	
+	public String motor;
+	
+	public Honda(){
+		
+	}
+	public Honda(String motor,String modelo,double preco) {
+		super(modelo,preco);
+		this.motor = motor;
+	}
 }
