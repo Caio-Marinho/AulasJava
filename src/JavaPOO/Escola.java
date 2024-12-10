488ba7a1c364ec8eee190651e836b61a3ed86664
Caio-Marinho <kaigabriel12@gmail.com>
2024-12-10 09:16:09 -0300

    Herança classe pessoa

--- src/JavaPOO/Escola.java
+++ src/JavaPOO/Escola.java
@@ -5,7 +5,9 @@ public class Escola {
 		Aluno estudante = new Aluno("Caio","12345678910","81979056770",7.90);
 		Professor docente = new Professor("Well","098765432101","81999930751",5000);
 		
+		estudante.Acordar();
 		estudante.Sofrer();
+		docente.Trabalhar();
 		docente.AplicarProva();
 	}
 }
